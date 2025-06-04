fb1fde8d751acb5f98a42c8cd34b4f65b832d8d9
Alesanchezcasanova23 <alesanchezcasanova23@gmail.com>
2025-06-04 10:25:20 -0400

    Update Dashboard_v1.py

--- Dashboard_v1.py
+++ Dashboard_v1.py
@@ -67,6 +67,17 @@ def combine_data(*dataframes_with_symbols):
 
     return combined_data
 
+# ======================================
+# RSI Calculation Function
+# ======================================
+def calculate_rsi(data, window=14):
+    delta = data['Close'].diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
+    rs = gain / loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
+
 # =======================================================================================================================
 # Streamlit title
 # =======================================================================================================================
